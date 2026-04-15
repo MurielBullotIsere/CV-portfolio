5a1211cbc913d7420da13f964168b51f1ea03b77
Muriel Bullot <murielbullot@hotmail.fr>
2026-04-15 15:59:47 +0200

    feat: responsive navbar - projet Allgamers

--- code/js/navbar.js
+++ code/js/navbar.js
@@ -1,9 +1,18 @@
 document.getElementById("navbar").innerHTML = `
-      <h2>Portfolio</h2>
-      <ul>
-      <li><a href="../../code/html/project_allgamers.html">Projet Allgamers</a></li>
-      <li><a href="../../index.html">CV en ligne</a></li>
-      </ul>
+    <div class="summary">
+        <div>
+            <h2>Portfolio</h2>
+            <ul>
+                <li><a href="../../code/html/project_allgamers.html">Projet Allgamers</a></li>
+                <li><a href="../../index.html">CV en ligne</a></li>
+            </ul>
+        </div>
+        <div class="logo-container logo-mobile">
+            <div class="logo">
+                <img src="../../assets/images/project_allgamers/manette.jpg" alt="Logo du jeu: manette d'une console" />
+            </div>
+        </div>
+    </div>   
 `;
 
   // Auto active le bon lien dans la navbar
